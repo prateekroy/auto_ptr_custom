e476e60eb37a97cfcb7cf9333710334a906f0cdb
codenrun <aman.coe@gmail.com>
2015-11-25 02:46:51 +0530

    Create auto_ptr_custom_test.cpp

---
+++ auto_ptr_custom_test.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "auto_ptr_custom.h"
+
+#define TRACE std::cout <<  __PRETTY_FUNCTION__  << ":"<<  __LINE__ <<  "\n"
+#define TEST std::cout << "-------------------------------------"<<  "\n";
+#define TEST_END std::cout << "================================ "<<  "\n";
+
+class Foo{
+public:
+	Foo(){
+		TRACE;	
+	}
+	~Foo(){
+		TRACE;	
+	}
+	
+	void hello(){
+		TRACE;
+		
+	}
+};
+
+class FooDeleter{
+public:
+	void operator()( Foo*obj){
+		TRACE;
+		delete obj;
+	}		
+};
+
+
+int main(){
+
+	
+	TEST
+	{	
+		typedef stdxx::auto_ptr_custom<Foo> FooPtr;
+
+		FooPtr fooPtr( new Foo);
+		fooPtr->hello();
+	}
+	
+	TEST
+	{	
+		typedef stdxx::auto_ptr_custom<Foo> FooPtr;
+
+		FooPtr  fooPtr( new Foo);
+		FooPtr  fooPtr2( fooPtr);
+		
+		//:TODO:
+		//FooPtr fooPtr = FooPtr( new Foo);   not wokring ... why??
+		
+		fooPtr2->hello();
+	}
+	TEST
+	{
+		typedef stdxx::auto_ptr_custom<Foo, FooDeleter> FooPtr;
+
+		FooPtr fooPtr( new Foo);
+		fooPtr->hello();
+	}
+	
+	TEST
+	{
+		typedef stdxx::auto_ptr_custom<Foo, FooDeleter> FooPtr;
+
+		FooPtr  fooPtr( new Foo);
+		FooPtr  fooPtr2( fooPtr);
+		
+		//:TODO:
+		//FooPtr fooPtr = FooPtr( new Foo);   not wokring ... why??
+		
+		fooPtr2->hello();
+	}	
+		
+	TEST_END;
+	
+	return 0;	
+}
